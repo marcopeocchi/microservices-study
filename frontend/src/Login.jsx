1a38e104a90c812643510164821829de18d42b65
marco <marco.piovanello@gmail.com>
2023-03-04 12:03:20 +0100

    better login screen

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,15 +1,16 @@
-import { useNavigate } from 'react-router-dom'
-import { useLogin } from './hooks/useLogin'
-import Logo from './components/Logo'
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Logo from './components/Logo'
+import { useLogin } from './hooks/useLogin'
 
 export default function Login() {
+  const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
   const login = useLogin()
 
   const performLogin = async () => {
-    await login('admin', password)
+    await login(username, password)
     navigate('/')
   }
 
@@ -21,13 +22,25 @@ export default function Login() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Logo />
-      <div className="mt-6 flex justify-center items-center">
-        <input className="bg-neutral-800 rounded-l text-xl h-10 w-64 text-center" type="password"
+      <Logo hideSubText />
+      <div className='mt-8 flex flex-col items-center'>
+        <input
+          placeholder='username'
+          name="fuu-user"
+          className="bg-neutral-800 rounded h-10 w-64 text-center"
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+          onKeyDown={detectEnterKey}
+        />
+        <input
+          placeholder='password'
+          name="fuu-pass"
+          className="mt-2 bg-neutral-800 rounded h-10 w-64 text-center "
+          type="password"
           onChange={(e) => setPassword(e.target.value)}
           onKeyDown={detectEnterKey}
         />
-        <button className="h-10 bg-blue-400 p-2 rounded-r" onClick={performLogin} >
+        <button className="mt-2 h-10 w-full font-semibold bg-blue-400 p-2 rounded" onClick={performLogin}>
           Login
         </button>
       </div>
--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,12 +1,12 @@
-export default function Logo() {
+export default function Logo({ hideSubText = false }) {
   return (
     <>
       <h1 className='pt-7 text-center font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-blue-600 to-sky-500'>
         Fuu
       </h1>
-      <div className='text-center text-xl text-neutral-100'>
+      {!hideSubText && <div className='text-center text-xl text-neutral-100'>
         Simple image viewer
-      </div>
+      </div>}
     </>
   )
 }
