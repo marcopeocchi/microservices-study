a271219c3c6cbeaa2f01eb991718b7345b7a468c
Marco Piovanello <marco.piovanello@gmail.com>
2023-03-07 19:26:50 +0000

    [go] minor changes in gallery service
    Signed-off-by: Marco Piovanello <marco.piovanello@gmail.com>

--- internal/gallery/repository.go
+++ internal/gallery/repository.go
@@ -126,8 +126,8 @@ func (r *Repository) FindByPath(ctx context.Context, path string) (domain.Conten
 		Source:        resOrig,
 		Avif:          resAvif,
 		WebP:          resWebp,
-		AvifAvailable: len(onlyImgs) == len(resAvif),
-		WebPAvailable: len(onlyImgs) == len(resWebp),
+		AvifAvailable: len(onlyImgs) >= len(resAvif),
+		WebPAvailable: len(onlyImgs) >= len(resWebp),
 	}
 
 	encoded, err := json.Marshal(content)
